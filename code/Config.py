680b4be95bc14e2c9b7dd895ee63787051bf6b05
leoimewore@yahoo.com <leoimewore@yahoo.com>
2026-03-22 03:58:13 +0000

    sql and spark notebooks

---
+++ code/Config.py
@@ -0,0 +1,18 @@
+BASE_PATH = "/Volumes/workspace/raw_crashes/source_system_crashes/"
+
+data = [
+  {
+    "path": f"{BASE_PATH}Traffic_Crashes_-_Crashes_20260319.csv",
+    "table": "crashes_table"
+  },
+
+  {
+    "path": f"{BASE_PATH}Traffic_Crashes_-_People_20260318.csv",
+    "table": "people_table"
+  },
+  {
+    "path": f"{BASE_PATH}Traffic_Crashes_-_Vehicles_20260318 (1).csv",
+    "table": "vehicles_table"
+  }
+]
+
